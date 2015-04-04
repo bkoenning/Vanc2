da366d6a0160df909f556cdf4cb58faa6d29a757
Rebecca Koenning <koenningmom@Rebeccas-Mac-mini.local>
2015-04-04 15:52:54 -0500

    Initial Commit

---
+++ Vanc2Tests/Vanc2Tests.m
@@ -0,0 +1,40 @@
+//
+//  Vanc2Tests.m
+//  Vanc2Tests
+//
+//  Created by Rebecca Koenning on 4/4/15.
+//  Copyright (c) 2015 Rebecca Koenning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Vanc2Tests : XCTestCase
+
+@end
+
+@implementation Vanc2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
